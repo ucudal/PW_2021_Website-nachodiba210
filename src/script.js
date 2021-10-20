458d328fecefddac5f637e9cbafdfec4ce16d862
Ignacio dibarrart <nachodibarrart@gmail.com>
2021-10-19 23:17:07 -0300

    object_creation

--- src/script.js
+++ src/script.js
@@ -4,3 +4,32 @@ function toggleModal(modalID) {
   document.getElementById(modalID).classList.toggle("flex");
   document.getElementById(modalID + "-backdrop").classList.toggle("flex");
 }
+
+let people = [];
+
+const addPerson = (event)=>{
+  event.preventDefault();
+  console.log('hola');
+  let person = {
+      id: people.length,
+      name: document.getElementById('name').value,
+      lastName: document.getElementById('last-name').value,
+      phone: document.getElementById('phone').value,
+      email: document.getElementById('email').value,
+      message: document.getElementById('message').value
+  }
+
+  people.push(person);
+  document.forms[0].reset();
+
+  console.log('added' , {people});
+  let pre = document.querySelector('#msg pre');
+  pre.textContent = '\n' + JSON.stringify(people, '\t', 2);
+
+  localStorage.setItem('PersonList', JSON.stringify(people));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('btn').addEventListener('click', addPerson);
+});
+
