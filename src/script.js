4c1b6032e53bc5a700247e06d2930cfc5682ab71
Ignacio dibarrart <nachodibarrart@gmail.com>
2021-11-15 21:25:56 -0300

    Added Get method for Work Experience

--- src/script.js
+++ src/script.js
@@ -4,27 +4,58 @@ function toggleModal(modalID) {
     document.getElementById(modalID).classList.toggle("flex");
     document.getElementById(modalID + "-backdrop").classList.toggle("flex");
 }
-var people = [];
-var addPerson = function (event) {
-    event.preventDefault();
-    var person = {
-        id: people.length,
-        name: document.getElementById('name').value,
-        lastName: document.getElementById('last-name').value,
-        phone: document.getElementById('phone').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    };
-    people.push(person);
-    document.forms[0].reset();
-    console.log('added', { people: people });
-    var pre = document.querySelector('#msg pre');
-    pre.textContent = '\n' + JSON.stringify(people, "\t", 2);
-    localStorage.setItem('PersonList', JSON.stringify(people));
-};
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('btn').addEventListener('click', addPerson);
-});
 function toggleDropdown() {
     document.getElementById("dropdown").classList.toggle("hidden");
 }
+function get_experiencia_laboral() {
+    var request = new XMLHttpRequest();
+    request.open("get", "https://PW2021-APINode-nachodiba210.nachodiba.repl.co/experiencia-laboral", true);
+    request.setRequestHeader("dataType", "jsonp");
+    request.setRequestHeader("crossDomain", "true");
+    request.onload = function () {
+        try {
+            var json = JSON.parse(request.responseText);
+            populate_portfolio(json);
+        }
+        catch (e) {
+            console.warn(e);
+        }
+    };
+    request.send();
+}
+function populate_portfolio(lista_experiencias) {
+    var portfolio_listing = document.getElementById("portfolio-listing");
+    // Remove all experiences
+    while (portfolio_listing.firstChild) {
+        portfolio_listing.removeChild(portfolio_listing.firstChild);
+    }
+    // Crear div con la empresa y su info adentro
+    var div = document.createElement("div");
+    div.classList.add("w-80");
+    lista_experiencias.experiencia_laboral.forEach(function (experiencia) {
+        // Crear empresa
+        var empresa = document.createElement("p");
+        empresa.textContent = experiencia.empresa;
+        empresa.classList.add("text-xl", "font-bold");
+        // Crear descripcion
+        var descripcion = document.createElement("p");
+        descripcion.textContent = experiencia.descripcion;
+        descripcion.classList.add("text-lg", "pl-6", "text-grey-400");
+        // Crear Puesto
+        var puesto = document.createElement("p");
+        puesto.textContent = experiencia.puesto;
+        puesto.classList.add("text-lg", "pl-6", "text-grey-400");
+        // Crear Fecha Inicio y fin
+        var inicio_fin = document.createElement("p");
+        inicio_fin.textContent = experiencia.fecha_inicio + "~" + experiencia.fecha_fin;
+        inicio_fin.classList.add("text-lg", "pl-6", "text-grey-300");
+        div.appendChild(empresa);
+        div.appendChild(descripcion);
+        div.appendChild(puesto);
+        div.appendChild(inicio_fin);
+    });
+    portfolio_listing.appendChild(div);
+}
+document.addEventListener("DOMContentLoaded", function () {
+    get_experiencia_laboral();
+});
