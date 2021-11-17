2cad08eae5497f3fc37c18b05f934bf3e9c75f35
Ignacio dibarrart <nachodibarrart@gmail.com>
2021-11-17 09:01:20 -0300

    POST method fix

--- src/script.js
+++ src/script.js
@@ -10,8 +10,6 @@ function toggleDropdown() {
 function get_experiencia_laboral() {
     var request = new XMLHttpRequest();
     request.open("get", "https://PW2021-APINode-nachodiba210.nachodiba.repl.co/experiencia-laboral", true);
-    request.setRequestHeader("dataType", "jsonp");
-    request.setRequestHeader("crossDomain", "true");
     request.onload = function () {
         try {
             var json = JSON.parse(request.responseText);
@@ -32,30 +30,57 @@ function populate_portfolio(lista_experiencias) {
     // Crear div con la empresa y su info adentro
     var div = document.createElement("div");
     div.classList.add("w-80");
-    lista_experiencias["experiencia-laboral"].forEach(function (experiencia) {
-        // Crear empresa
-        var empresa = document.createElement("p");
-        empresa.textContent = experiencia.empresa;
-        empresa.classList.add("text-xl", "font-bold");
-        // Crear descripcion
-        var descripcion = document.createElement("p");
-        descripcion.textContent = experiencia.descripcion;
-        descripcion.classList.add("text-lg", "pl-6", "text-grey-400");
-        // Crear Puesto
-        var puesto = document.createElement("p");
-        puesto.textContent = experiencia.puesto;
-        puesto.classList.add("text-lg", "pl-6", "text-grey-400");
-        // Crear Fecha Inicio y fin
-        var inicio_fin = document.createElement("p");
-        inicio_fin.textContent = experiencia.fechaInicio + "~" + experiencia.fechaFin;
-        inicio_fin.classList.add("text-lg", "pl-6", "text-grey-300");
-        div.appendChild(empresa);
-        div.appendChild(descripcion);
-        div.appendChild(puesto);
-        div.appendChild(inicio_fin);
-    });
+    if (lista_experiencias["experiencia_laboral"]) {
+        lista_experiencias["experiencia_laboral"].forEach(function (experiencia) {
+            // Crear empresa
+            var empresa = document.createElement("p");
+            empresa.textContent = experiencia.empresa;
+            empresa.classList.add("text-xl", "font-bold");
+            // Crear descripcion
+            var descripcion = document.createElement("p");
+            descripcion.textContent = experiencia.descripcion;
+            descripcion.classList.add("text-lg", "pl-6", "text-grey-400");
+            // Crear Puesto
+            var puesto = document.createElement("p");
+            puesto.textContent = experiencia.puesto;
+            puesto.classList.add("text-lg", "pl-6", "text-grey-400");
+            // Crear Fecha Inicio y fin
+            var inicio_fin = document.createElement("p");
+            inicio_fin.textContent = experiencia.fechaInicio + "~" + experiencia.fechaFin;
+            inicio_fin.classList.add("text-lg", "pl-6", "text-grey-300");
+            div.appendChild(empresa);
+            div.appendChild(descripcion);
+            div.appendChild(puesto);
+            div.appendChild(inicio_fin);
+        });
+    }
     portfolio_listing.appendChild(div);
 }
+document.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var toSend = {
+        name: document.getElementById("name").value,
+        last_name: document.getElementById("last_name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        phone: document.getElementById("phone").value
+    };
+    var jsonString = JSON.stringify(toSend);
+    var url = "https://PW2021-APINode-nachodiba210-2.nachodiba.repl.co/enviar-formulario";
+    fetch(url, {
+        method: "POST",
+        body: jsonString,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        console.log(response);
+    }).then(function (html) {
+        console.log(html);
+    })["catch"](function (error) {
+        console.error(error);
+    });
+});
 document.addEventListener("DOMContentLoaded", function () {
     get_experiencia_laboral();
 });
--- src/script.ts
+++ src/script.ts
@@ -12,8 +12,6 @@ function toggleDropdown() {
 function get_experiencia_laboral() {
   const request = new XMLHttpRequest();
   request.open("get", "https://PW2021-APINode-nachodiba210.nachodiba.repl.co/experiencia-laboral", true);
-  request.setRequestHeader("dataType", "jsonp");
-  request.setRequestHeader("crossDomain", "true");
   request.onload = () => {
       try {
           const json = JSON.parse(request.responseText);
@@ -37,35 +35,65 @@ function populate_portfolio(lista_experiencias) {
   // Crear div con la empresa y su info adentro
   const div = document.createElement("div");
   div.classList.add("w-80");
-  lista_experiencias["experiencia-laboral"].forEach(experiencia => {
-    // Crear empresa
-    const empresa = document.createElement("p");
-    empresa.textContent = experiencia.empresa;
-    empresa.classList.add("text-xl", "font-bold");
+  if (lista_experiencias["experiencia_laboral"]) {
+    lista_experiencias["experiencia_laboral"].forEach(experiencia => {
+      // Crear empresa
+      const empresa = document.createElement("p");
+      empresa.textContent = experiencia.empresa;
+      empresa.classList.add("text-xl", "font-bold");
 
-    // Crear descripcion
-    const descripcion = document.createElement("p");
-    descripcion.textContent = experiencia.descripcion;
-    descripcion.classList.add("text-lg", "pl-6", "text-grey-400");
+      // Crear descripcion
+      const descripcion = document.createElement("p");
+      descripcion.textContent = experiencia.descripcion;
+      descripcion.classList.add("text-lg", "pl-6", "text-grey-400");
 
-    // Crear Puesto
-    const puesto = document.createElement("p");
-    puesto.textContent = experiencia.puesto;
-    puesto.classList.add("text-lg", "pl-6", "text-grey-400");
+      // Crear Puesto
+      const puesto = document.createElement("p");
+      puesto.textContent = experiencia.puesto;
+      puesto.classList.add("text-lg", "pl-6", "text-grey-400");
 
-    // Crear Fecha Inicio y fin
-    const inicio_fin = document.createElement("p");
-    inicio_fin.textContent = experiencia.fechaInicio + "~" + experiencia.fechaFin;
-    inicio_fin.classList.add("text-lg", "pl-6", "text-grey-300");
+      // Crear Fecha Inicio y fin
+      const inicio_fin = document.createElement("p");
+      inicio_fin.textContent = experiencia.fechaInicio + "~" + experiencia.fechaFin;
+      inicio_fin.classList.add("text-lg", "pl-6", "text-grey-300");
 
-    div.appendChild(empresa);
-    div.appendChild(descripcion);
-    div.appendChild(puesto);
-    div.appendChild(inicio_fin);
-  });
+      div.appendChild(empresa);
+      div.appendChild(descripcion);
+      div.appendChild(puesto);
+      div.appendChild(inicio_fin);
+    });
+  }
 
   portfolio_listing.appendChild(div);
 }
+
+document.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const toSend = {
+    name: (<HTMLInputElement>document.getElementById("name")).value,
+    last_name: (<HTMLInputElement>document.getElementById("last_name")).value,
+    email: (<HTMLInputElement>document.getElementById("email")).value,
+    message: (<HTMLInputElement>document.getElementById("message")).value,
+    phone: (<HTMLInputElement>document.getElementById("phone")).value
+  }
+
+  const jsonString = JSON.stringify(toSend);
+
+  const url = "https://PW2021-APINode-nachodiba210-2.nachodiba.repl.co/enviar-formulario";
+  fetch(url, {
+      method : "POST",
+      body: jsonString,
+      headers: {
+      'Content-Type': 'application/json'
+      },
+  }).then((response) => {
+    console.log(response);
+  }).then((html) => {
+    console.log(html);
+  }).catch((error) => {
+    console.error(error);
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   get_experiencia_laboral();
